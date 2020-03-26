320c62625f44ec2596e1eb9efeae5a755b8074c9
Matthew Jones <matts3371@gmail.com>
2020-03-26 17:20:19 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_studebts.rb
+++ db/migrate/001_create_studebts.rb
@@ -1,6 +1,6 @@
 class create_students < ActiveRecord::Migration[6.0]
   def change do 
-    create_table :students |t| do 
+    create_table :students do |t|  
       t.first_name :string 
       t.last_name :string 
     end 
